c2afe400946ed778427a2d0933e8ca7a6fbe7bb0
Jennx-x <jennls.prog@gmail.com>
2025-12-06 22:25:54 -0500

    Fix route configuration

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from controllers import main_controller
 app = Flask(__name__)
 
 # Rutas
-app.route('/')(main_controller.index)
-app.route('/analyze', methods=['POST'])(main_controller.analyze)
+app.add_url_rule('/', 'index', main_controller.index)
+app.add_url_rule('/analyze', 'analyze', main_controller.analyze, methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
